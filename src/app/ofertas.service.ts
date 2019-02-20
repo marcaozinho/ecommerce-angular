5c665fef05d6cecab698c8427169bebb5fe8192e
marcaozinho <aci.marcus@gmail.com>
2019-02-19 21:30:37 -0300

    add some features

--- src/app/ofertas.service.ts
+++ src/app/ofertas.service.ts
@@ -20,25 +20,25 @@ export class OfertasService {
     public getOfertas(): Promise<Oferta[]> {
         return this.http.get(`${URL_API}/ofertas?destaque=true`)
         .toPromise()
-        .then((resposta: any) => resposta.json())
+        .then((resposta: Response) => resposta.json())
     }
 
     public getOfertasPorCategoria(categoria:string): Promise<Oferta[]> {
         return this.http.get(`${URL_API}/ofertas?categoria=${categoria}`)
         .toPromise()
-        .then((resposta: any) => resposta.json())
+        .then((resposta: Response) => resposta.json())
     }
 
     public getOfertasPorId(id:number): Promise<Oferta> {
         return this.http.get(`${URL_API}/ofertas?id=${id}`)
         .toPromise()
-        .then((resposta: any) => resposta.json()[0])
+        .then((resposta: Response) => resposta.json()[0])
     }
 
     public getComoUsarOfertaPorId(id:number): Promise<String> {
         return this.http.get(`${URL_API}/como-usar?id=${id}`)
         .toPromise()
-        .then((resposta: any) => {
+        .then((resposta: Response) => {
             return resposta.json()[0].descricao
         })
     }
@@ -46,7 +46,7 @@ export class OfertasService {
     public getOndeFicaOfertaPorId(id:number): Promise<String> {
         return this.http.get(`${URL_API}/onde-fica?id=${id}`)
         .toPromise()
-        .then((resposta: any) => {
+        .then((resposta: Response) => {
             return resposta.json()[0].descricao
         })
     }
@@ -54,7 +54,7 @@ export class OfertasService {
     public pesquisaOfertas(termo: string): Observable<Oferta[]> {
         return this.http.get(`${URL_API}/ofertas?descricao_oferta_like=${termo}`)
             .retry(10)
-            .map((resposta: any) => resposta.json())
+            .map((resposta: Response) => resposta.json())
     }
 
     // .then((resposta: any) => resposta.json().shift())
--- src/app/topo/topo.component.ts
+++ src/app/topo/topo.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { OfertasService } from '../ofertas.service'
-import { Observable } from 'rxjs';
+import { Observable, Observer, Subject, observable } from 'rxjs';
 import { Oferta } from '../shared/oferta.model';
+import 'rxjs/Rx'
+import 'rxjs/add/operator/switchMap'
 
 @Component({
   selector: 'app-topo',
@@ -12,20 +14,64 @@ import { Oferta } from '../shared/oferta.model';
 export class TopoComponent implements OnInit {
 
   public ofertas: Observable<Oferta[]>
+  private subjectPesquisa: Subject<string> = new Subject<string>()
 
   constructor(private ofertasService: OfertasService) { }
 
   ngOnInit() {
+
+    //retorno oferta[]
+    this.ofertas = this.subjectPesquisa
+      .debounceTime(1000) //executa a ação do switchmap após 1 segundo
+      .switchMap((termoDaBusca: string) => {
+        console.log('requisicao http para api')
+        if(termoDaBusca.trim() === '') {
+          //retornar um observable de array de ofertas vazio
+          return Observable.of<Oferta[]>([])
+        }
+        
+        return this.ofertasService.pesquisaOfertas(termoDaBusca)
+      })
+    
+    this.ofertas.subscribe((ofertas: Oferta[]) => console.log(ofertas)) //nao tem problema o subscribe ficar aqui, uma vez que está em watch
+
+    // let tempo = Observable.interval(2000)
+    // tempo.subscribe((intervalo:number) => { console.log (intervalo) } )
+
+    // Parametros do subscribe pq é o observador
+    // Observavel = fonte de dados que é produzidos de forma assíncrona
+
+    // Observável
+    // let meuObservableTeste = Observable.create((observer: Observer<string>) => {
+    //   observer.next('Primeiro evento da stream')
+    //   observer.next('Segundo evento da stream')
+    //   observer.complete()
+    //   observer.error('Erro')
+    //   observer.next('Terceiro evento da stream')
+    // })
+
+    // Observador
+    // meuObservableTeste.subscribe(
+    //   (resultado: any) => console.log(resultado),
+    //   (error: any) => console.log(error),
+    //   () => console.log("Stream de eventos finalizada!")     
+    // )
   }
 
+  // event: Event
+  // public pesquisa(termoDaBusca: string): void  {
+  //   // console.log((<HTMLInputElement>event.target).value)
+  //   this.ofertas = this.ofertasService.pesquisaOfertas(termoDaBusca)
+  //   this.ofertas.subscribe(
+  //     (data: Oferta[]) => console.log(data),
+  //     (erro: any) => console.log('Erro status: ', erro.status),
+  //     () => console.log('Fluxo de eventos completo!')
+  //   )
+  // }
+
   public pesquisa(termoDaBusca: string): void  {
-    // console.log((<HTMLInputElement>event.target).value)
-    this.ofertas = this.ofertasService.pesquisaOfertas(termoDaBusca)
-    this.ofertas.subscribe(
-      (data: Oferta[]) => console.log(data),
-      (erro: any) => console.log('Erro status: ', erro.status),
-      () => console.log('Fluxo de eventos completo!')
-    )
+    console.log('Keyup:', termoDaBusca)
+    this.subjectPesquisa.next(termoDaBusca)
   }
 
 }
